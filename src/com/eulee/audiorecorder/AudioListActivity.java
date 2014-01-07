c944b38bc1c90d80cd844b7b547a40b862a5a310
euleeteh <euleerules@gmail.com>
2014-01-08 10:24:42 +1300

    filtered backup files out of the list

--- src/com/eulee/audiorecorder/AudioListActivity.java
+++ src/com/eulee/audiorecorder/AudioListActivity.java
@@ -4,14 +4,17 @@ package com.eulee.audiorecorder;
 
 import android.app.Activity;
 import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Environment;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Button;
 
 import java.io.File;
+import java.io.FilenameFilter;
 
 public class AudioListActivity extends ListActivity {
 	@Override
@@ -22,10 +25,13 @@ public class AudioListActivity extends ListActivity {
         AppLog.logString("Going through the directory to list files");
 
         String filePath = Environment.getExternalStorageDirectory().getPath();
+        FilenameFilter filter = new fileExtensionFilter();
         File file = new File(filePath,"AudioRecorder");
 
-        File[] fileList = file.listFiles();
+        File[] fileList = file.listFiles(filter);
+
         String[] theNamesOfFiles = new String[fileList.length];
+
         for (int i = 0; i < theNamesOfFiles.length; i++) {
             theNamesOfFiles[i] = fileList[i].getName();
             AppLog.logString(theNamesOfFiles[i]);
@@ -42,5 +48,37 @@ public class AudioListActivity extends ListActivity {
             AppLog.logString("Done. Return to Main page");
 			}
 			});
+
+
+
+        /*// selecting single ListView item
+        ListView lv = getListView();
+        // listening to single listitem click
+        lv.setOnItemClickListener(new OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                // getting listitem index
+                int songIndex = position;
+
+                // Starting new intent
+                Intent in = new Intent(getApplicationContext(),
+                        AndroidBuildingMusicPlayerActivity.class);
+                // Sending songIndex to PlayerActivity
+                in.putExtra("songIndex", songIndex);
+                setResult(100, in);
+                // Closing PlayListView
+                finish();
+            }
+        });*/
 	}
+    /**
+     * Class to filter files which are having .mp3 extension
+     * */
+    class fileExtensionFilter implements FilenameFilter {
+        public boolean accept(File dir, String name) {
+            return (name.endsWith(".wav") || name.endsWith(".WAV"));
+        }
+    }
 }
